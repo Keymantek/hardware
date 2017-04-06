27caac1669a91a03572e20c3e7b7417758bee172
li <li@aihlp.com>
2017-04-06 14:31:08 +0800

    添加SPI部分

--- src/com/keymantek/serialport/utils/TesamSPI.java
+++ src/com/keymantek/serialport/utils/TesamSPI.java
@@ -9,39 +9,20 @@ public class TesamSPI
 	}
 	private final String DEBUG_TAG = "ccy";
 
-	public int Close()
-	{
-		return deInit();
-	}
+	protected native int deInit();
 
-	public native int deInit();
+	protected native int init();
 
-	public native int init();
+	protected native int recvData(byte[] data, int offset, int length);
 
-	public int Open()
-	{
-		return init();
-	}
+	protected native int sendData(byte[] data, int offset, int length);
 
-	public int Read(byte[] data, int offset, int length)
-	{
-		return recvData(data, offset, length);
-	}
+	protected native int setBits(int bits);
 
-	public native int recvData(byte[] data, int offset, int length);
+	protected native int setFreq(int freq);
 
-	public native int sendData(byte[] data, int offset, int length);
+	protected native int setIOControl(int diy);
 
-	public native int setBits(int bits);
+	protected native int setMode(int mode);
 
-	public native int setFreq(int freq);
-
-	public native int setIOControl(int diy);
-
-	public native int setMode(int mode);
-
-	public int Write(byte[] data, int offset, int length)
-	{
-		return sendData(data, offset, length);
-	}
 }
