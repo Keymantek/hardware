25ccff59ce2d09ca78cf87840cbf9bae1e5f410f
yang <15608215960@163.com>
2017-07-17 13:32:12 +0800

    增加SPI动态库，新增64位动态库

--- src/com/keymantek/serialport/utils/TesamSPI.java
+++ src/com/keymantek/serialport/utils/TesamSPI.java
@@ -1,28 +1,39 @@
 package com.keymantek.serialport.utils;
 
-public class TesamSPI
-{
 
-	static
-	{
+public class TesamSPI {
+
+	private final String    DEBUG_TAG  = "ccy"; 
+	public int Open(){
+		return init();
+	}
+	
+	public int Write(byte[] data,int offset, int length){
+		return sendData( data,  offset, length);
+	}
+	
+	public int Read(byte[] data,int offset, int length){
+		return recvData( data, offset, length);
+	}
+	
+	public int  Close(){
+		return deInit();
+	}
+		
+	private native int init();
+		
+	private native  int deInit();
+		
+	public native int sendData(byte[] data,int offset, int length);
+			
+	public native int recvData(byte[] data,int offset, int length);
+	
+	public native int setMode(int mode);
+	public native int setFreq(int freq);
+	public native int setBits(int bits);
+	public native int setIOControl(int diy);
+	
+	static {
 		System.loadLibrary("TesamSPI");
 	}
-	private final String DEBUG_TAG = "ccy";
-
-	protected native int deInit();
-
-	protected native int init();
-
-	protected native int recvData(byte[] data, int offset, int length);
-
-	protected native int sendData(byte[] data, int offset, int length);
-
-	protected native int setBits(int bits);
-
-	protected native int setFreq(int freq);
-
-	protected native int setIOControl(int diy);
-
-	protected native int setMode(int mode);
-
 }
